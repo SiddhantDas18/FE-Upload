54d410bf28d451ab8d8bddbe1c7d1f54216eb27d
siddhantdas18 <siddhantdas18@gmail.com>
2025-05-26 16:15:36 +0530

    Updated ui

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true, // This makes the server accessible on your local network
-    // You can also specify a port if you don't want to use the default (5173)
-    // port: 3000,
+    host: true, 
   }
 })
